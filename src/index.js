60c57802cfa5076429cbb0d66dd9902fa3d9d927
Esteban Ramirez <esteban@dorfjungs.com>
2020-01-22 12:37:16 -0600

    demo with lottieweb

--- src/index.js
+++ src/index.js
@@ -1,18 +1,1 @@
 import './styles/style.scss';
-import * as animationData from './scripts/animation.json';
-// console.log(animationData.default)
-const params = {
-  container: document.getElementById('animation'),
-  renderer: 'svg',
-  loop: false,
-  autoplay: false,
-  animationData: animationData.default
-};
-
-const anim = lottie.loadAnimation(params);
-setTimeout(() => {
-  anim.play()
-  anim.onComplete = () => {
-      anim.playSegments([120, 200], false);
-  }
-}, 2000)
